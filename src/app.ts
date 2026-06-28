59a5833ff16a213ab1066e142598250bacaeedb6
ShafiulTuhin <tuhin6488@gmail.com>
2026-06-28 16:21:11 +0600

    craete user data and send to prisma studio

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
+import httpStatus from "http-status";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 const app: Application = express();
 
@@ -10,8 +13,50 @@ app.use(cors({ origin: config.app_url, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.send("Hello world");
+});
+
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+
+  const isUserExist = await prisma.user.findUnique({ where: { email } });
+  if (isUserExist) {
+    throw new Error("Email already exist");
+  }
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  const profile = await prisma.profile.create({
+    data: {
+      userId: createUser.id,
+      profilePhoto,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createUser.id,
+      email: createUser.email || email,
+    },
+  });
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User created successfully!",
+    data: { user },
+  });
 });
 
 export default app;
